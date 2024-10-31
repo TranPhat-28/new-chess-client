d7bb20cbf9a68cf1d2766a75dd5b31d5f3de009d
TranPhat-28 <tphat8301@gmail.com>
2024-10-31 17:24:50 +0700

    feat: Implement skeleton UI for Shop page

--- src/pages/Shop/index.tsx
+++ src/pages/Shop/index.tsx
@@ -1,5 +1,27 @@
 const ShopPage = () => {
-    return <div className="h-full w-full text-white">Shop is coming soon</div>;
+    return (
+        <div className="page-content-wrapper">
+            <div className="page-preset overflow-y-scroll gap-3 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+                <div className="bg-blue-300 w-full h-40">A</div>
+                <div className="bg-blue-300 w-full h-40">A</div>
+                <div className="bg-blue-300 w-full h-40">A</div>
+                <div className="bg-blue-300 w-full h-40">A</div>
+                <div className="bg-blue-300 w-full h-40">A</div>
+                <div className="bg-blue-300 w-full h-40">A</div>
+                <div className="bg-blue-300 w-full h-40">A</div>
+                <div className="bg-blue-300 w-full h-40">A</div>
+                <div className="bg-blue-300 w-full h-40">A</div>
+                <div className="bg-blue-300 w-full h-40">A</div>
+                <div className="bg-blue-300 w-full h-40">A</div>
+                <div className="bg-blue-300 w-full h-40">A</div>
+                <div className="bg-blue-300 w-full h-40">A</div>
+                <div className="bg-blue-300 w-full h-40">A</div>
+                <div className="bg-blue-300 w-full h-40">A</div>
+                <div className="bg-blue-300 w-full h-40">A</div>
+                
+            </div>
+        </div>
+    );
 };
 
 export default ShopPage;
