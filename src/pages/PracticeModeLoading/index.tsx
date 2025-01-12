2ad7ada1ea4e96bbe2b68c19350dbdc621b8adf8
TranPhat-28 <tphat8301@gmail.com>
2025-01-12 18:05:57 +0700

    feat: Implement delete saved game and start new game

--- src/pages/PracticeModeLoading/index.tsx
+++ src/pages/PracticeModeLoading/index.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { HashLoader } from "react-spinners";
 import { RootState } from "../../redux/store";
@@ -11,6 +11,40 @@ import { toast } from "react-toastify";
 const PracticeModeLoadingPage = () => {
     const navigate = useNavigate();
     const token = useSelector((state: RootState) => state.auth.token);
+    const [gameLoading, setGameLoading] = useState<boolean>(true);
+    const [alertPlayer, setAlertPlayer] = useState<boolean>(false);
+
+    // Button handler
+    const startNewGameHandler = () => {
+        // Delete old progress then start a new game
+        axios
+            .delete("/api/PracticeMode/Saved", {
+                headers: { Authorization: `Bearer ${token}` },
+            })
+            .then((response) => {
+                // If success
+                toast.success(response.data.message);
+                navigate("/practice/game");
+            })
+            .catch((error) => {
+                // Show toast
+                toast.error(error.message);
+                // Show alert
+                showCustomAlert(
+                    "Error",
+                    "Oops! We cannot start a new game right now",
+                    "Home",
+                    () => navigate("/main/lobby"),
+                    undefined,
+                    <FaRegCircleXmark size={"5rem"} color={"red"} />,
+                    true
+                );
+            });
+    };
+
+    const resumeGameHandler = () => {
+        // Load old progress
+    };
 
     useEffect(() => {
         // Check if any saved game exists
@@ -23,8 +57,8 @@ const PracticeModeLoadingPage = () => {
                 toast.success(response.data.message);
                 if (response.data.data === true) {
                     // If saved game found
-                    console.log("SAVED GAME FOUND");
-                    console.log(response);
+                    setGameLoading(false);
+                    setAlertPlayer(true);
                 } else {
                     // If no saved game found, go to play page
                     navigate("/practice/game");
@@ -48,14 +82,42 @@ const PracticeModeLoadingPage = () => {
 
     return (
         <div className="h-full w-full flex flex-col justify-center items-center bg-subtle object-contain p-4">
-            <div className="bg-base-100 w-full max-w-xs rounded-lg p-4">
-                <h3 className="font-bold text-xl text-center">
-                    Loading game data
-                </h3>
-                <div className="flex justify-center pt-4">
-                    <HashLoader />
+            {gameLoading && (
+                <div className="bg-base-100 w-full max-w-xs rounded-lg p-4">
+                    <h3 className="font-bold text-xl text-center">
+                        Loading game data
+                    </h3>
+                    <div className="flex justify-center pt-4">
+                        <HashLoader />
+                    </div>
                 </div>
-            </div>
+            )}
+
+            {alertPlayer && (
+                <div className="bg-base-100 w-full max-w-lg rounded-lg p-4">
+                    <h3 className="font-bold text-xl text-center">
+                        Saved game found!
+                    </h3>
+                    <div className="flex pt-4">
+                        Would you like to resume the previous game or start a
+                        new game?
+                    </div>
+                    <div className="flex pt-4 flex-row justify-center gap-2">
+                        <button
+                            className="btn btn-primary"
+                            onClick={resumeGameHandler}
+                        >
+                            Resume
+                        </button>
+                        <button
+                            className="btn btn-primary btn-outline"
+                            onClick={startNewGameHandler}
+                        >
+                            New game
+                        </button>
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
